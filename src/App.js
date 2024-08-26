1b5247f754d7c7646558d16cb1b27a742e06e51c
PraveenPolnati <praveenpolnati@gmail.com>
2024-08-26 11:21:49 +0530

    added some text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1>3 x 3 Matrix </h1>
+        <p>When you click on a box - color change to green</p>
+        <p>When you click on the last box, all colors change to orange in sequence of their clicks</p>
         <Matrix />
       </div>
     );
